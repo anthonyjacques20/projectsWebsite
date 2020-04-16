770ba5c39d9e1d292d1119f9bd12435284c72a2d
Anthony Jacques <anthonyjacques20@gmail.com>
2020-04-15 17:17:35 -0700

    Add before_request function to get all of the posts so that they are accessible in the navbar

--- flaskr/db.py
+++ flaskr/db.py
@@ -100,7 +100,7 @@ def init_db_command():
     click.echo('Initialized the database!')
 
 def init_app(app):
-    #Call the close_db function when cleaning upa fter returning the response
+    #Call the close_db function when cleaning up after returning the response
     app.teardown_appcontext(close_db)
     #Add a new command that can be called with the flask command
     app.cli.add_command(init_db_command)
