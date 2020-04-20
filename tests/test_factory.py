3e8d5dffe4727250032336c109b3358b673b0921
Anthony Jacques <anthony@silanano.com>
2020-04-20 16:34:10 -0700

    Move the SQLAlchemy track modifications to the top level configs so that it is always present.  Delete the test_db as the code was SQLite specific.  Add some parameters to get rid of warnings in test_factory.py

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,8 +1,15 @@
 from hobbyProjectWebsite import create_app
 
 def test_config():
+    db_path = 'postgresql:///hobbyprojectwebsite_test'
+
     assert not create_app().testing
-    assert create_app({'TESTING': True}).testing
+    assert create_app(
+        {
+            'TESTING': True,
+            'SQLALCHEMY_DATABASE_URI': db_path,
+        }
+    ).testing
 
 #Example test that checks what we received is correct
 def test_hello(client):
