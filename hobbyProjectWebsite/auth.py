574d9bd17e7197f49c8da89bc432dd4cac9eada8
Anthony Jacques <anthonyjacques20@gmail.com>
2020-04-20 13:30:23 -0700

    Refactor auth.py to use database functions and change to postgres and sqlalchemy functions

--- hobbyProjectWebsite/auth.py
+++ hobbyProjectWebsite/auth.py
@@ -6,7 +6,7 @@ from flask import (
 
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from hobbyProjectWebsite.models import db
+from hobbyProjectWebsite.models import db, Project, User
 
 bp = Blueprint('auth', __name__, url_prefix="/auth")
 
@@ -21,18 +21,17 @@ def register():
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif getUser(username) is not None:
             error = 'User {} is already registered.'.format(username)
 
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
+            user = User(
+                username = username,
+                password = generate_password_hash(password)
             )
+            db.session.add(user)
+            db.session.commit()
             #Must call db.commit() to save the changese made in the insert above
-            db.commit()
             return redirect(url_for('auth.login'))
 
         flash(error)
@@ -45,9 +44,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+        user = getUser(username)
 
         if user is None:
             error = 'Incorrect username.'
@@ -75,15 +72,33 @@ def load_logged_in_user():
     if user_id is None:
         g.user = None
     else:
-        g.user = db.execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone()
+        g.user = getUserByID(user_id)
 
 @bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
 
+#Create a function to return the user
+def getUser(username):
+    user = User.query.filter(User.username == username).first()
+    if user:
+        columns = ['id', 'username', 'password']
+        #Return the dictionary
+        return {columns[i]: getattr(user, columns[i]) for i in range(0,len(user)) }
+    else:
+        return None
+    
+#Create a function to return the user by id
+def getUserByID(id):
+    user = User.query.filter(User.id == id).first()
+    if user:
+        columns = ['id', 'username', 'password']
+        #Return the dictionary
+        return {columns[i]: getattr(user, columns[i]) for i in range(0,len(user)) }
+    else:
+        return None
+    
 #This function checks to see if the user is logged in
 #If a user is logged in, then it continues to the view
 #If the user is NOT logged in, the it redirects to the login page
