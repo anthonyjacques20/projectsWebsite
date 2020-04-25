8e6475e580abbcdc3704c0405c8f4de271873adc
Anthony Jacques <anthonyjacques20@gmail.com>
2020-04-24 18:01:07 -0700

    Add get_comments function.  Also finish the POST route for creating comments

--- hobbyProjectWebsite/comment.py
+++ hobbyProjectWebsite/comment.py
@@ -3,7 +3,7 @@ import functools
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-
+from datetime import datetime
 from werkzeug.security import check_password_hash, generate_password_hash
 from hobbyProjectWebsite.auth import login_required
 from hobbyProjectWebsite.db import db
@@ -20,6 +20,7 @@ def create(id):
         #Grab the id from the url_prefix
         project_id = id
         author_id = g.user['id']
+        error = None
 
         if not text:
             error = 'Comment text is required.'
@@ -27,13 +28,24 @@ def create(id):
         if error is not None:
             flash(error)
         else:
-            Comment(
+            comment = Comment(
                 text = text,
                 project_id = project_id,
-                author_id = author_id
+                author_id = author_id,
+                created = datetime.now()
             )
+            #Add and commit the comment
+            db.session.add(comment)
+            db.session.commit()
 
         #Redirect to the show page with parameters
         return redirect(url_for('project.show', id=id))
     else:
         return "You are in the create comment page for id: " + str(id)
+
+
+def get_comments(projectID):
+    #Join the users table and replace the user id with the username from the users table
+    comments = Comment.query.join(User, Comment.author_id==User.id).add_columns(Comment.text, Comment.created, User.username).filter(Comment.project_id==projectID).order_by(Comment.created).all()
+    print(comments)
+    return comments
