c7f1be7a094a96f040266d8fe90d18b0dbacfa60
Anthony Jacques <anthonyjacques20@gmail.com>
2020-05-04 12:47:59 -0700

    Fix path after adding url prefix

--- tests/test_project.py
+++ tests/test_project.py
@@ -19,7 +19,7 @@ def test_index(client, auth):
     assert b'by test on 2018-01-01' in response.data
     assert b'test body' in response.data
     #Check for the edit button since the logged in user wrote this test project
-    assert b'href="/1/edit"' in response.data
+    assert b'href="/projects/1/edit"' in response.data
 
 @pytest.mark.parametrize('path',(
     '/projects/create',
